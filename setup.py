a67442ae0c36bf813bd4bada39d914694bbf645b
bw2 <weisburd@broadinistitute.org>
2021-04-11 17:51:13 -0400

    update minor version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ install_requires = [
 
 setup(
     name='reviewer2',
-    version="0.9.7",
+    version="0.9.8",
     description="Starts a simple image server that lets you quickly flip through image files from a local directory "
                 "using your web browser and optionally answering customizable questions about each one",
     install_requires=install_requires,
