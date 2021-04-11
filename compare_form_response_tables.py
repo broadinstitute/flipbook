dd1d0e34e6790bcb1c8642a784738da3be2875bc
bw2 <weisburd@broadinistitute.org>
2021-04-11 09:00:04 -0400

    finished implementing. Print all stats

--- compare_form_response_tables.py
+++ compare_form_response_tables.py
@@ -5,6 +5,11 @@ import pandas as pd
 EXPECTED_COLUMNS = {"Path", "Verdict", "Confidence"}
 HIGH_CONFIDENCE = "high"
 
+DISCORDANT_VERDICT_COLUMN = "Discordant Verdict"
+DISCORDANT_SCORE_COLUMN = "Discordance Score"
+DISCORDANT_TEXT_COLUMN = "Discordance Text"
+
+
 def parse_args():
     p = argparse.ArgumentParser(description="Check concordance between two form response tables created by 2 users "
         "reviewing the same images. Rows in the two tables are matched by their same Path column.")
@@ -53,41 +58,37 @@ def compute_discordance_columns_func(suffix1, suffix2):
     confidence_column1 = f"Confidence_{suffix1}"
     confidence_column2 = f"Confidence_{suffix2}"
 
-    discordant_verdict_column = "Discordant Verdict"
-    score_column = "Discordance Score"
-    text_column="Discordance Text"
-
     def compute_discordance_columns(row):
         if not row[verdict_column1] or not row[verdict_column2]:
             return row
 
         if row[verdict_column1] == row[verdict_column2]:
             # same verdict
-            row[discordant_verdict_column] = 0
-            row[score_column] = 0
-            row[text_column] = "same verdict"
+            row[DISCORDANT_VERDICT_COLUMN] = 0
+            row[DISCORDANT_SCORE_COLUMN] = 0
+            row[DISCORDANT_TEXT_COLUMN] = "same verdict"
             if row[confidence_column1] and row[confidence_column2]:
                 if row[confidence_column1] == row[confidence_column2]:
-                    row[score_column] = 0
-                    row[text_column] = "same verdict, same confidence"
+                    row[DISCORDANT_SCORE_COLUMN] = 0
+                    row[DISCORDANT_TEXT_COLUMN] = "same verdict, same confidence"
                 else:
-                    row[score_column] = 1
-                    row[text_column] = "same verdict, different confidence"
+                    row[DISCORDANT_SCORE_COLUMN] = 1
+                    row[DISCORDANT_TEXT_COLUMN] = "same verdict, different confidence"
         else:
             # different verdicts
-            row[discordant_verdict_column] = 1
-            row[score_column] = 2
-            row[text_column] = "different verdict"
+            row[DISCORDANT_VERDICT_COLUMN] = 1
+            row[DISCORDANT_SCORE_COLUMN] = 2
+            row[DISCORDANT_TEXT_COLUMN] = "different verdict"
             if row[confidence_column1] or row[confidence_column2]:
                 if row[confidence_column1] == HIGH_CONFIDENCE and row[confidence_column2] == HIGH_CONFIDENCE:
-                    row[score_column] = 4
-                    row[text_column] = "different verdict, both high confidence"
+                    row[DISCORDANT_SCORE_COLUMN] = 4
+                    row[DISCORDANT_TEXT_COLUMN] = "different verdict, both high confidence"
                 elif row[confidence_column1] == HIGH_CONFIDENCE or row[confidence_column2] == HIGH_CONFIDENCE:
-                    row[score_column] = 3
-                    row[text_column] = "different verdict, one high confidence"
+                    row[DISCORDANT_SCORE_COLUMN] = 3
+                    row[DISCORDANT_TEXT_COLUMN] = "different verdict, one high confidence"
                 else:
-                    row[score_column] = 2
-                    row[text_column] = "different verdict, zero high confidence"
+                    row[DISCORDANT_SCORE_COLUMN] = 2
+                    row[DISCORDANT_TEXT_COLUMN] = "different verdict, zero high confidence"
 
         return row
 
@@ -96,26 +97,37 @@ def compute_discordance_columns_func(suffix1, suffix2):
 
 def main():
     args, df1, df2 = parse_args()
-    df_joined = df1.join(df2, lsuffix=f"_{args.suffix1}", rsuffix=f"_{args.suffix2}").reset_index()
+    df_joined = df1.join(df2, lsuffix=f"_{args.suffix1}", rsuffix=f"_{args.suffix2}", how="outer").reset_index()
     df_joined = df_joined.fillna('')
 
-    #  print some stats
-    df1_verdicts_counter = sorted(collections.Counter(df1['Verdict']).items())
+    #  print stats about input tables
+    print("-"*20)
+    df1_verdicts_counter = ", ".join([f"{count:2d} {key}" for key, count in sorted(collections.Counter(df1['Verdict']).items())])
     df1_num_verdicts = sum(df1['Verdict'].str.len() > 0)
     df1_num_high_confidence = collections.Counter(df1['Confidence']).get(HIGH_CONFIDENCE, 0)
     df1_high_confidence_fraction = df1_num_high_confidence / df1_num_verdicts
 
-    df2_verdicts_counter = sorted(collections.Counter(df2['Verdict']).items())
+    df2_verdicts_counter = ", ".join([f"{count:2d} {key}" for key, count in sorted(collections.Counter(df2['Verdict']).items())])
     df2_num_verdicts = sum(df2['Verdict'].str.len() > 0)
     df2_num_high_confidence = collections.Counter(df2['Confidence']).get(HIGH_CONFIDENCE, 0)
     df2_high_confidence_fraction = df2_num_high_confidence / df2_num_verdicts
 
-    print(f"{args.table2}:  {df1_num_verdicts} verdicts: {df1_verdicts_counter}. High confidence for {100*df1_high_confidence_fraction:4.1f}% of them")
-    print(f"{args.table1}:  {df2_num_verdicts} verdicts: {df2_verdicts_counter}. High confidence for {100*df2_high_confidence_fraction:4.1f}% of them")
+    print(f"{args.table2}:  {df1_num_verdicts} verdicts. {df1_verdicts_counter}. High confidence for {100*df1_high_confidence_fraction:4.1f}% of them")
+    print(f"{args.table1}:  {df2_num_verdicts} verdicts. {df2_verdicts_counter}. High confidence for {100*df2_high_confidence_fraction:4.1f}% of them")
 
     #  compute concordance
     df_joined = df_joined.apply(compute_discordance_columns_func(args.suffix1, args.suffix2), axis=1)
 
+    # print concordance stats
+    print("-"*20)
+    num_discordant_verdicts = sum(df_joined[DISCORDANT_VERDICT_COLUMN])
+    print(f"{num_discordant_verdicts} out of {len(df_joined)} ({100*num_discordant_verdicts/len(df_joined):0.1f}%) of "
+          f"verdicts differed between the two tables")
+    print(f"\nDiscordance score = {sum(df_joined[DISCORDANT_SCORE_COLUMN])}:")
+    print("\n".join([f"{count:2d} review comparisons: {key}" for key, count in sorted(collections.Counter(df_joined[DISCORDANT_TEXT_COLUMN]).items())]))
+
+    # output table to tsv
+    print("-"*20)
     df_joined.to_csv(args.output_tsv, header=True, sep="\t", index=False)
     print(f"Wrote {len(df_joined)} rows to {args.output_tsv}")
 
